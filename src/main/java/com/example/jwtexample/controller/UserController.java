2f3acca8d1b62708f8a57528e04188e1a5c4e211
Mahmut Emin Yavuz <91795112+itsaymey@users.noreply.github.com>
2025-05-12 16:36:19 +0300

    JWT Filter Class added

--- src/main/java/com/example/jwtexample/controller/UserController.java
+++ src/main/java/com/example/jwtexample/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -13,9 +15,19 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @GetMapping("/getAll")
+    public List<User> getAll()
+    {
+        return userService.getAll();
+    }
+
     @PostMapping("/register")
     public User register(@RequestBody User user){
         return userService.register(user);
+    }
 
+    @PostMapping("/login")
+    public  String login( @RequestBody User user ){
+        return userService.verify(user);
     }
 }
--- src/main/java/com/example/jwtexample/service/UserService.java
+++ src/main/java/com/example/jwtexample/service/UserService.java
@@ -3,20 +3,40 @@ package com.example.jwtexample.service;
 import com.example.jwtexample.model.User;
 import com.example.jwtexample.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class UserService {
 
     @Autowired
-    UserRepository repo;
+    private UserRepository repo;
+    @Autowired
+    private AuthenticationManager manager;
+    @Autowired
+    private JWTService jwtService;
     private BCryptPasswordEncoder encoder= new BCryptPasswordEncoder(12);
     @Transactional
     public User register(User user)
     {
      user.setPassword(encoder.encode(user.getPassword()));
     return repo.save(user);
+    }
+    public String verify(User user ){
+        Authentication authentication= manager.authenticate
+                (new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword()));
+        if(authentication.isAuthenticated())
+        return jwtService.generateToken(user.getUsername());
+        else
+            return "Fail";
+    }
+    public List<User> getAll(){
+        return repo.findAll();
     }
 }
